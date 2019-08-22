4cbfd95aea14d5697f982e3c2b30e3951589e51f
Oleg Aleynikov <aleynikov@bit-it.ru>
2019-08-22 11:06:41 +0300

    origin

---
+++ Test Git/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test_Git.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult GetAll()
+        {
+            return View(); 
+        }
+    }
+}
